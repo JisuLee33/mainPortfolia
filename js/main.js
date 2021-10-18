d6d3d4bd6c1adf366d037ab01354d339f64d554a
LAPTOP-4546K4R8\koaaoffice <leejisu33@gmail.com>
2021-10-18 13:51:58 +0900

    nameChange

--- js/main.js
+++ js/main.js
@@ -7,13 +7,60 @@ $(function(){
   nav.innerHTML += "<li><a href='#"+i+"'>"+list[i]+"</a></li>";
   i++
   }
+  
+  //nav hamburg menu
+  if(window.innerWidth<480){
+    $("#fa-bars").on("click",function(){
+      $(this).hide();
+      $("#nav").show();
+    });
+    $("#nav li").on("click",function(){
+      $("#nav").hide();
+      $("#fa-bars").show();
+    });
+  }
 
-  $("#fa-bars").on("click",function(){
-    $(this).hide();
-    $("#nav").show();
-  });
-  $("#nav li").on("click",function(){
-    $("#nav").hide();
-    $("#fa-bars").show();
-  });
+  //popup 
+  class Pop{
+    constructor(text){
+      this.text= text;
+    }
+
+    popOpen(){
+      const newLocal = [`./img/${i}.png`,];
+      let pop1 = newLocal
+      let i = 0;
+      while(i<pop1.length){
+        
+        $("#poject"+i).on("click",function(){
+          const selectId = document.getElementById("popup");
+      selectId.innerHTML = pop1[i];
+        });
+        
+      }
+    }
+  }
+    
+    const pop = new Pop();
+      pop.popOpen();
+
+
+    
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
